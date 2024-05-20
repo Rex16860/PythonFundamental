d4b8d2930c3062c5bd5d0ed18940e06875a6c420
Rex16860 <dwireza1002@gmail.com>
2024-05-20 16:57:53 +0700

    For Loop Push-up Program

---
+++ Loop.py
@@ -0,0 +1,14 @@
+"""
+For Loop Push-up Program
+"""
+
+print('Bro : "gotta push-up 10 times')
+pushUp = 0
+target = 10
+
+for pushUp in range(pushUp,target + 1) :
+    if pushUp == 0:
+        continue
+    print(f'current push-up : {pushUp}')
+
+print('well done')
