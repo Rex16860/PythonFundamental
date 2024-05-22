23294ffc173e28fe1f1c8f86cc4595ded69ad5ab
Rex16860 <dwireza1002@gmail.com>
2024-05-22 13:15:14 +0700

    Basic Syntax

--- PythonStatement.py
+++ PythonStatement.py
@@ -5,3 +5,14 @@ Statement
 print(1)
 print("hello world")
 fruit = "Apple"
+
+# Block Statement
+
+if fruit == "Apple":
+    print("it's a fruit")
+    print("and it's tasty")
+
+
+# Function Statement
+def greeting():
+    print("Hello World")
