89baed68d01917c5247c686e5d56b54cae4de763
Rex16860 <dwireza1002@gmail.com>
2024-05-23 11:18:26 +0700

    Boolean and coparison

---
+++ BooleanAndComparisonOperator.py
@@ -0,0 +1,27 @@
+# Boolean
+print(True and True)
+print(True and False)
+print(False and False)
+print(True or False)
+
+# comparison
+print(100 == 100)
+print(100 != 100)
+print(100 != 50)
+print(70 == 50)
+print(300 >= 300)
+print(300 <= 500)
+
+print(100 > 99 or 5 != 5)
+
+v, v2, v3 = "John", "finn", "sean"
+
+print('o' in v)
+print('a' not in v2)
+print('sea' in v3)
+
+# Chain Comparison Operator
+print(11 <= 22 != 4 and 77 > 43 < 45)
+print(11 <= 22 == 4 and 77 > 43 < 45)
+
+print('j' in v and 'inn' in v2 or 7 >= 4 != 5 and 200 < 201)
