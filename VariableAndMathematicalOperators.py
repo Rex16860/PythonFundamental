cf7e7d00537dd8374687d2b421320f5462de3955
Rex16860 <dwireza1002@gmail.com>
2024-05-23 10:55:48 +0700

    variable and math operator

---
+++ VariableAndMathematicalOperators.py
@@ -0,0 +1,29 @@
+x = 100
+
+print(x + 5)
+print(x - 5)
+print(x / 5)
+print(x * 5)
+
+print(4*4*4)
+print(4**3)
+
+a, b, c = 30, 20, 40.01
+print(a)
+print(b)
+print(c)
+
+d = (a + b - c) * 2
+print(d)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+v = 5
+print(80 / v)
+print(80 // v)
+print(80 % v)
+print(80 - (v * 8))
